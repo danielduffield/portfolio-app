031d2e04ab38e6db827eb958a2eb9110123cab94
Daniel Duffield <danieljduffield@gmail.com>
2017-10-11 13:35:38 -0700

    Served the contents of the public directory as static middleware via Express. #5

--- server/index.js
+++ server/index.js
@@ -3,4 +3,6 @@ const app = express()
 
 app.listen(3000, () => console.log('Listening on 3000...'))
 
+app.use(express.static('server/public'))
+
 app.get('/', (req, res) => res.send('Hello world!'))
