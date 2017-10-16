64004d426899ede3a4e775eed47787f7d3a5b2fc
Daniel Duffield <danieljduffield@gmail.com>
2017-10-16 13:11:44 -0700

    Moved project information into an array of objects and changed the render method to render a preview for each project in the array. #9

--- client/utils/home.js
+++ client/utils/home.js
@@ -1,5 +1,17 @@
 import React from 'react'
 
+const projects = [
+  {
+    name: 'TrackSlash Audio Ripper',
+    url: 'https://www.github.com/danielduffield/trackslash-audio-ripper',
+    repo: 'https://www.github.com/danielduffield/trackslash-audio-ripper',
+    description: 'A web app for music fans who want to slice audio from a Youtube video into tracks for download.',
+    languages: 'JavaScript, HTML, CSS',
+    tech: 'Node.js, Express.js, Socket.io, Bootstrap, RESTful API, Git',
+    image: 'images/trackslash-demo-image.png'
+  }
+]
+
 export default class Home extends React.Component {
   render() {
     return (
@@ -22,25 +34,29 @@ export default class Home extends React.Component {
         <div className="row demo-projects-container">
           <div className="col-sm-10 col-sm-offset-1">
             <div className="project-demos-container">
-              <div className="project-demo">
-                <a href="https://www.github.com/danielduffield/trackslash-audio-ripper" target="_blank" rel="noopener noreferrer">
-                  <div className="demo-img-wrapper"></div>
-                </a>
-                <div className="demo-details">
-                  <a className="demo-title-link" href="https://www.github.com/danielduffield/trackslash-audio-ripper" target="_blank" rel="noopener noreferrer">
-                    <h3 className="demo-title">TrackSlash Audio Ripper</h3>
-                  </a>
-                  <p className="demo-description">A web app for music fans who want to slice audio from a Youtube video into tracks for download.</p>
-                  <div className="demo-sub-description">
-                    <p className="demo-lang">
-                      <span className="demo-subtitle">Languages: </span> JavaScript, HTML, CSS
-                    </p>
-                    <p className="demo-tech">
-                      <span className="demo-subtitle">Technologies: </span> Node.js, Express.js, Socket.io, Bootstrap, RESTful API, Git
-                    </p>
+              {projects.map((project, index) => {
+                return (
+                  <div key={index} className="project-demo">
+                    <a href={project.url} target="_blank" rel="noopener noreferrer">
+                      <div className="demo-img-wrapper"></div>
+                    </a>
+                    <div className="demo-details">
+                      <a className="demo-title-link" href={project.url} target="_blank" rel="noopener noreferrer">
+                        <h3 className="demo-title">{project.name}</h3>
+                      </a>
+                      <p className="demo-description">{project.description}</p>
+                      <div className="demo-sub-description">
+                        <p className="demo-lang">
+                          <span className="demo-subtitle">Languages: </span> {project.languages}
+                        </p>
+                        <p className="demo-tech">
+                          <span className="demo-subtitle">Technologies: </span> {project.tech}
+                        </p>
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </div>
+                )
+              })}
             </div>
           </div>
         </div>
