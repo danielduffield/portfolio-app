4a10eee8bf82ff00cc14cb3e3cd156a2df08cf2c
Daniel Duffield <danieljduffield@gmail.com>
2017-10-16 12:00:08 -0700

    Imported react and react-dom to set up a render using app.js. #9

--- client/main.js
+++ client/main.js
@@ -1,1 +1,17 @@
-console.log('Hello world!')
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './utils/app.js'
+
+const $root = document.getElementById('root')
+
+render()
+
+function render() {
+  ReactDOM.render(
+    <div>
+      <App />
+    </div>,
+    $root
+  )
+}
