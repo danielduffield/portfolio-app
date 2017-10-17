09175be11d06cd26bcc549fea1acbd2bcc7b6304
Daniel Duffield <danieljduffield@gmail.com>
2017-10-17 13:08:12 -0700

    Added the Resume component to the app render. #11

--- client/utils/app.js
+++ client/utils/app.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import Home from './home.js'
+import Resume from './resume.js'
 
 export default function App() {
   return (
     <div>
       <Home />
+      <Resume />
     </div>
   )
 }
