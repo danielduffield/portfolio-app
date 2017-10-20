9faa9f2e4eb51939cee4a7fc86895e2b5193869e
Daniel Duffield <danieljduffield@gmail.com>
2017-10-20 10:18:11 -0700

    Added a background image to the About page with a darkening layer. #15

--- client/utils/about.js
+++ client/utils/about.js
@@ -1,15 +1,35 @@
 import React from 'react'
+import styled from 'styled-components'
 import { connect } from 'react-redux'
 
 class About extends React.Component {
   render() {
     return (
       <div className={'row' + (this.props.view === 'about' ? '' : ' hidden')}>
-        <div>About</div>
+        <AboutSplash></AboutSplash>
+        <DarkeningLayer />
       </div>
     )
   }
 }
+
+const AboutSplash = styled.div`
+  height: 100%;
+  width: 100%;
+  position: fixed;
+  top: 50px;
+  background-image: url('images/about-splash.jpg');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+`
+
+const DarkeningLayer = styled.div`
+  height: 100%;
+  width: 100%;
+  position: fixed;
+  background-color: rgba(0, 0, 0, 0.4);
+`
 
 function mapStateToProps(state) {
   return {
