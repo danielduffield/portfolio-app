3519051fc032a28754cd56ca424d5ca442b248bd
Daniel Duffield <danieljduffield@gmail.com>
2017-10-20 11:04:00 -0700

    Arranged the elements of the About page with positioning for description text. #15

--- client/utils/about.js
+++ client/utils/about.js
@@ -12,11 +12,31 @@ class About extends React.Component {
           <AboutTitleContainer>
             <AboutTitle>About</AboutTitle>
           </AboutTitleContainer>
+          <AboutImage></AboutImage>
+          <AboutDetails>
+            <AboutDescription>
+              <p>All work and no play makes Daniel a good programmer. All work and no play makes Daniel a good programmer.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </AboutDescription>
+          </AboutDetails>
         </AboutWrapper>
       </div>
     )
   }
 }
+
+const AboutDetails = styled.span`
+  padding: 50px 50px 50px 0;
+  float: left;
+  font-size: 1.5em;
+`
+
+const AboutDescription = styled.div`
+  width: 100% - 400px;
+  padding-right: 50px;
+  height: 100%;
+  position: absolute;
+`
 
 const AboutSplash = styled.div`
   height: 100%;
@@ -37,19 +57,32 @@ const DarkeningLayer = styled.div`
 `
 
 const AboutWrapper = styled.div`
-  background-color: aqua;
+  background-color: rgba(250, 250, 250, 0.9);
   position: relative;
-  top: 50px;
-  height: 550px;
+  top: 100px;
+  height: 500px;
   padding: 0;
 `
 
 const AboutTitleContainer = styled.div`
+  height: 100px;
   text-align: center;
 `
 
-const AboutTitle = styled.h3`
+const AboutTitle = styled.span`
+  line-height: 100px;
   font-size: 2.5em;
+`
+
+const AboutImage = styled.div`
+  height: 300px;
+  width: 300px;
+  background-color: palegoldenrod;
+  margin: 50px;
+  background-image: url('images/DD-3.jpg');
+  background-size: cover;
+  display: inline-block;
+  float: left;
 `
 
 function mapStateToProps(state) {
