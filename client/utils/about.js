2e8561fbcb894159ed374d07a7710fb610d6cdca
Daniel Duffield <danieljduffield@gmail.com>
2017-10-20 10:20:12 -0700

    Created HTML elements for the About component render with appropriate styling. #15

--- client/utils/about.js
+++ client/utils/about.js
@@ -8,6 +8,11 @@ class About extends React.Component {
       <div className={'row' + (this.props.view === 'about' ? '' : ' hidden')}>
         <AboutSplash></AboutSplash>
         <DarkeningLayer />
+        <AboutWrapper className="col-sm-8 col-sm-offset-2">
+          <AboutTitleContainer>
+            <AboutTitle>About</AboutTitle>
+          </AboutTitleContainer>
+        </AboutWrapper>
       </div>
     )
   }
@@ -29,6 +34,22 @@ const DarkeningLayer = styled.div`
   width: 100%;
   position: fixed;
   background-color: rgba(0, 0, 0, 0.4);
+`
+
+const AboutWrapper = styled.div`
+  background-color: aqua;
+  position: relative;
+  top: 50px;
+  height: 550px;
+  padding: 0;
+`
+
+const AboutTitleContainer = styled.div`
+  text-align: center;
+`
+
+const AboutTitle = styled.h3`
+  font-size: 2.5em;
 `
 
 function mapStateToProps(state) {
