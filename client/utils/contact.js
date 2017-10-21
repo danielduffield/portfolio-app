1cf3e064b2b9da9e7da87dbae32903c8ca92bf95
Daniel Duffield <danieljduffield@gmail.com>
2017-10-21 14:03:29 -0700

    Added and positioned HTML elements for the Contact form and description. #16

--- client/utils/contact.js
+++ client/utils/contact.js
@@ -8,10 +8,68 @@ class About extends React.Component {
       <div id="about-section" className={'row' + (this.props.view === 'contact' ? '' : ' hidden')}>
         <ContactSplash></ContactSplash>
         <DarkeningLayer />
+        <ContactWrapper className="col-sm-6 col-sm-offset-3">
+          <FormWrapper>
+            <DescriptionWrapper>
+              <div>If you have any:</div>
+              <ContactQueryLink className="btn btn-default col-sm-4">questions</ContactQueryLink>
+              <ContactQueryLink className="btn btn-default col-sm-4">feedback</ContactQueryLink>
+              <ContactQueryLink className="btn btn-default col-sm-4">job info</ContactQueryLink>
+              <p>or exceptional cases, please feel free to reach out to me on LinkedIn, via email, or via the form below.</p>
+            </DescriptionWrapper>
+            <ContactForm>
+              <InputWrapper>
+                <ContactInput />
+              </InputWrapper>
+              <SubmitButton className="btn btn-default">Submit</SubmitButton>
+            </ContactForm>
+          </FormWrapper>
+        </ContactWrapper>
       </div>
     )
   }
 }
+
+const ContactQueryLink = styled.button`
+  margin: 10px 0;
+`
+
+const DescriptionWrapper = styled.div`
+  width: 500px;
+  padding: 10px 20px;
+  margin: 0 auto;
+  font-size: 1.15em;
+`
+
+const InputWrapper = styled.div`
+
+`
+
+const FormWrapper = styled.div`
+  text-align: center;
+`
+
+const ContactForm = styled.form`
+  height: 350px;
+`
+
+const SubmitButton = styled.button`
+  position: relative;
+`
+
+const ContactInput = styled.textarea`
+  height: 200px;
+  width: 500px;
+  resize: none;
+`
+
+const ContactWrapper = styled.div`
+  background-color: rgba(250, 250, 250, 0.8);
+  position: relative;
+  top: 100px;
+  height: 400px;
+  padding: 0;
+`
 
 const ContactSplash = styled.div`
   height: 100%;
